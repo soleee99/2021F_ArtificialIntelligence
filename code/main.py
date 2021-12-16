b0099495faa81cd4d267408e5a0e6a1cbdc46035
soleee99 <notredamejin@naver.com>
2021-12-16 22:35:56 +0900

    passing all levels

--- code/main.py
+++ code/main.py
@@ -1,9 +1,9 @@
 from world import *
 import random
-random.seed(905)
+random.seed(900)
 user = User(move='v3')
 
-level = 3
+level = 0
 world = World(user=user, level=level)
 TEST_STEP = 101
 # train
